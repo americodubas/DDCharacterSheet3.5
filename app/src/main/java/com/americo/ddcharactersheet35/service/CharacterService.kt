9dc60745fc18e87ed1c364240c4ccdf12f994263
Americo <americo.dubas@gmail.com>
2017-08-19 20:52:58 -0300

    Insert documentation

--- app/src/main/java/com/americo/ddcharactersheet35/service/CharacterService.kt
+++ app/src/main/java/com/americo/ddcharactersheet35/service/CharacterService.kt
@@ -14,24 +14,32 @@ import java.util.*
 /**
  * Created by Americo on 27/05/2017.
  *
- * Service responsible for the [Character]
+ * Service responsible for the [Character].
  */
 class CharacterService(val context: Context) {
 
     private val characterDao = CharacterDao(context)
 
+    /**
+     * Returns a [Character] by [id].
+     */
     fun getCharacter(id: String): Character {
         return characterDao.getCharacter(id.toInt())
     }
 
+    /**
+     * Returns a [List] of all [CharacterDto].
+     */
     fun getAllCharacters(): List<CharacterDto> {
         return convertFromTo(characterDao.getAllCharacters())
     }
 
+    /**
+     * Insert an empty [Character] with default values.
+     */
     fun insertEmptyCharacter(): Int {
-        //TODO create blank char
         var character = Character()
-        character.name = "Hizagkuur"
+        character.name = "Unknown"
         character.race = getDefaultRace()
         character = characterDao.insertCharacter(character)
 
@@ -44,22 +52,40 @@ class CharacterService(val context: Context) {
         return character.id
     }
 
+    /**
+     * Returns the default [Race].
+     */
     private fun getDefaultRace() = characterDao.getRace(2)
 
+    /**
+     * Returns the default [Class].
+     */
     private fun getDefaultClass() = characterDao.getClasses(1)
 
+    /**
+     * Update the [Character].
+     */
     fun updateCharacter(character: Character) {
         characterDao.updateCharacter(character)
     }
 
+    /**
+     * Returns a [List] of all [Race].
+     */
     fun getAllRaces(): List<Race> {
         return characterDao.getAllRaces()
     }
 
+    /**
+     * Returns a [List] of all [Classes].
+     */
     fun getAllClasses(): List<Classes> {
         return characterDao.getAllClasses()
     }
 
+    /**
+     * Returns a [List] of the [Classes] not used by the [Character].
+     */
     fun getNotUsedClasses(character: Character): List<Classes> {
         val allClasses = getAllClasses()
         val usedClassesId = TreeSet<String>()
@@ -67,24 +93,41 @@ class CharacterService(val context: Context) {
         return allClasses.filter { !usedClassesId.contains(it.id.toString()) }
     }
 
+    /**
+     * Returns a [List] of [Classes] of the [Character] by it's [id].
+     */
     fun getCharacterClasses(id: String): List<Classes> {
         return characterDao.getCharacterClasses(id.toInt())
     }
 
+    /**
+     * Insert the relation of [CharacterClasses].
+     */
     fun insertCharacterClasses(characterClasses: CharacterClasses){
         characterDao.insertCharacterClass(characterClasses)
     }
 
+    /**
+     * Delete the relation of [CharacterClasses].
+     */
     fun deleteCharacterClasses(characterClasses: CharacterClasses){
         characterDao.deleteCharacterClass(characterClasses)
     }
 
+    /**
+     * Increase the level of the [CharacterClasses] by + 1.
+     */
     fun increaseCharacterClassesLevel(characterClasses: CharacterClasses) : CharacterClasses {
         characterClasses.level += 1
         characterDao.updateCharacterClass(characterClasses)
         return characterClasses
     }
 
+    /**
+     * Decrease the level of the [CharacterClasses] by - 1.
+     * The level won't be decreased if the [CharacterClasses] level is 1,
+     * a [CannotDecreaseCharacterClassesLevel] will be throw.
+     */
     fun decreaseCharacterClassesLevel(characterClasses: CharacterClasses) : CharacterClasses {
         if (!canDecreaseLevel(characterClasses)) throw CannotDecreaseCharacterClassesLevel()
         characterClasses.level -= 1
@@ -92,6 +135,9 @@ class CharacterService(val context: Context) {
         return characterClasses
     }
 
+    /**
+     * Verify if the [CharacterClasses] can be decreased, the minimum level is 1.
+     */
     private fun canDecreaseLevel(characterClasses: CharacterClasses) = characterClasses.level > 1
 
 }
